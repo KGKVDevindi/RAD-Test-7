ac8ebb9b3902485f49f9465c40358625d0066247
ADMIN <ADMIN@VISH>
2024-01-28 23:07:23 +0530

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a comment
+            Console.WriteLine("Hello World!");
+            /* The code below will print the words Hello World to the screen, and it is amazing*/
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
